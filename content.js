438afd85d94ec3ad628aac46d248c0c39ea15b51
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-12-20 14:00:37 +0530

    popup script

--- content.js
+++ content.js
@@ -1,12 +1,12 @@
 function getArticleText(){
     const article= document.querySelector('article');
     if(article) return article.innerText;
-    const paragraphs = Arrays.from(document.querySelectorAll('p'));
+    const paragraphs = Array.from(document.querySelectorAll('p'));
     return paragraphs.map(p=>p.innerText).join('\n');
 }
 
 chrome.runtime.onMessage.addListener((req, _sender, sendResponse)=>{
-    if(req.type= 'GET_ARTICLE_TEXT'){
+    if(req.type=== 'GET_ARTICLE_TEXT'){
         const text = getArticleText();
         sendResponse({text});
     }
--- popup.js
+++ popup.js
@@ -1,3 +1,29 @@
 document.getElementById("summarize").addEventListener("click", ()=>{
-    console.log("Summarize clicked");
-})
+    const result = document.getElementById("result");
+    result.textContent="Extracting text... ";
+
+    chrome.tabs.query({active:true,currentWindow:true},([tab])=>{
+        if (!tab || !tab?.id) {
+            result.textContent = "No active tab.";
+            return;
+          }
+        chrome.tabs.sendMessage(
+            tab.id,
+            {type: 'GET_ARTICLE_TEXT'},
+            (response) => {
+               
+                if (chrome.runtime.lastError) {
+                  result.textContent = "Content script not available. Reload the page.";
+                  return;
+                }
+        
+                if (!response || !response.text || !response.text.trim()) {
+                  result.textContent = "No article text found.";
+                  return;
+                }
+        
+                result.textContent = response.text.slice(0, 300) + "...";
+              }
+        );
+    });
+});
