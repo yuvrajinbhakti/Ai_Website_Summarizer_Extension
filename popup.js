eb2576a93a985f31878bbab31353ac549546b864
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-12-22 00:06:22 +0530

    copy button

--- popup.js
+++ popup.js
@@ -102,3 +102,19 @@ async function getGeminiSummary(rawText, type, apiKey){
     return data.candidates?.[0]?.content?.parts?.[0]?.text ?? "No summary." ;
 
 }
+
+document.getElementById("copy-btn").addEventListener('click',()=>{
+    const txt=document.getElementById("result").innerText;
+    if(!txt) return;
+
+    navigator.clipboard.writeText(txt).then(()=>{
+        const btn=document.getElementById("copy-btn");
+        const old = btn.textContent;
+        btn.textContent="Copied!";
+
+        setTimeout(()=>{
+            btn.textContent = old
+        },2000);
+    })
+
+})
